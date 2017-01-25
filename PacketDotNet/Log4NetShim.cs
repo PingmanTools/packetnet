2a97d83c717d120b8bc2bf32cd3ed648795c9ccb
Matthew Little <matt@pingman.com>
2017-01-25 14:40:54 -0700

    Remove log4net dependency, replaced with shim

---
+++ PacketDotNet/Log4NetShim.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log4net
+{
+   public class Log : ILog
+   {
+      public void Debug(string msg)
+      {
+#if DEBUG
+         Console.WriteLine(msg);
+#endif
+      }
+
+      public void DebugFormat(string msg, params object[] p)
+      {
+#if DEBUG
+         Console.WriteLine(msg, p);
+#endif
+      }
+   }
+
+   public interface ILog
+   {
+      void Debug(string msg);
+      void DebugFormat(string msg, params object[] p);
+   }
+
+   public interface ILogInactive : ILog { }
+
+   public class LogManager
+   {
+      public static ILog GetLogger(Type type)
+      {
+         return new Log();
+      }
+   }
+}
+
+namespace log4net.Core
+{
+
+}
